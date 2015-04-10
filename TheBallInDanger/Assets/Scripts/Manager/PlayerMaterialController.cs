dee19fb2a55d3565fe6a053a54ef3f6a5e1cf732
Hamza Abdullah <monosoft54@gmail.com>
2015-04-09 19:27:47 -0700

    Fifth
    Bug Fixes!

--- TheBallInDanger/Assets/Scripts/Manager/PlayerMaterialController.cs
+++ TheBallInDanger/Assets/Scripts/Manager/PlayerMaterialController.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerMaterialController : MonoBehaviour {
 
@@ -29,9 +30,6 @@ public class PlayerMaterialController : MonoBehaviour {
 	void Start () 
 	{
 		ballMaterial = ballDefault;
-		
-		
-		player.GetComponent<Renderer>().material = ballMaterial;
 		ballDefault = GameController.control.ballDefault;
 
 	}
@@ -40,5 +38,51 @@ public class PlayerMaterialController : MonoBehaviour {
 	{
 		player = GameObject.FindGameObjectWithTag ("Player");
 		player.GetComponent<Renderer>().material = ballMaterial;
+
+		if (ballMaterial.name == "BallC") 
+		{
+			if(GameObject.FindGameObjectWithTag ("Check1"))
+			{
+				GameObject.FindGameObjectWithTag ("Check1").GetComponent<Image>().enabled = true;
+			}
+			if(GameObject.FindGameObjectWithTag ("Check2"))
+			{
+				GameObject.FindGameObjectWithTag ("Check2").GetComponent<Image>().enabled = false;
+			}
+			if(GameObject.FindGameObjectWithTag ("Check3"))
+			{
+				GameObject.FindGameObjectWithTag ("Check3").GetComponent<Image>().enabled = false;
+			}
+		}
+		if (ballMaterial.name == "BallC1") 
+		{
+			if(GameObject.FindGameObjectWithTag ("Check1"))
+			{
+				GameObject.FindGameObjectWithTag ("Check1").GetComponent<Image>().enabled = false;
+			}
+			if(GameObject.FindGameObjectWithTag ("Check2"))
+			{
+				GameObject.FindGameObjectWithTag ("Check2").GetComponent<Image>().enabled = true;
+			}
+			if(GameObject.FindGameObjectWithTag ("Check3"))
+			{
+				GameObject.FindGameObjectWithTag ("Check3").GetComponent<Image>().enabled = false;
+			}
+		}
+		if (ballMaterial.name == "BallC3") 
+		{
+			if(GameObject.FindGameObjectWithTag ("Check1"))
+			{
+				GameObject.FindGameObjectWithTag ("Check1").GetComponent<Image>().enabled = false;
+			}
+			if(GameObject.FindGameObjectWithTag ("Check2"))
+			{
+				GameObject.FindGameObjectWithTag ("Check2").GetComponent<Image>().enabled = false;
+			}
+			if(GameObject.FindGameObjectWithTag ("Check3"))
+			{
+				GameObject.FindGameObjectWithTag ("Check3").GetComponent<Image>().enabled = true;
+			}
+		}
 	}
 }
---
+++ TheBallInDanger/Assets/Scripts/Player/TouchingJump.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchingJump : MonoBehaviour {
+
+	[SerializeField] public bool isFalling = false;
+	private Rigidbody m_Rigidbody;
+
+	// Update is called once per frame
+	public void OnPointerUp() 
+	{
+		m_Rigidbody = GameObject.FindGameObjectWithTag ("Player").GetComponent<Rigidbody>();
+
+		if (isFalling == false)
+		{
+			m_Rigidbody.AddForce(Vector3.up * 7, ForceMode.Impulse);
+		}
+		isFalling = true;
+	}
+
+	public void OnCollisionStay ()
+	{
+		isFalling = false;	
+	}
+}
